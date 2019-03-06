c36223d1850076701a89be400c5f3f6c4a491626
Rashad Abdul-Salaam <rabdulsalaam@gmail.com>
2019-03-06 16:29:04 -0500

    Begin React frontend and package.json updates for Parcel Bundler

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<div>Cryptochain in React!: 3rd Live Update</div>, document.getElementById('root'));
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const path = require('path');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
@@ -24,6 +25,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
@@ -75,6 +77,12 @@ app.get('/api/wallet-info', (req, res) => {
     address,
     balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
   });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'client/dist/index.html')
+  );
 });
 
 const syncWithRootState = () => {
